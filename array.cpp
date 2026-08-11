24deb1cfb5e2cb19c6b725d1581c2c9065be5527
Khushi Yadav <yadavkailash33@gmail.com>
2026-08-11 21:26:52 +0530

    Added array implementation

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std ;
+int main() {
+   int arr[5] = {1,2,3,4,5};
+   int n= 5;
+   int maxSum = INT_MIN;
+   for(int st=0; st<n; st++) {
+     int pcurrSum = 0;
+   for(int end=st; end<n; end++) {
+   currSum = currSum + arr[end];
+   maxSum = max(currSum , maxSum);
+   }
+   
+   }
+   cout<<"Maximum Subarray Sum :"<<maxSum<<endl;
+   
+   
+   return 0;
+ }
